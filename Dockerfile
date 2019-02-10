2a4163268f9abfadaff9f4f797db15b479530077
Adama DIOP <adama.diop@capgemini.com>
2019-02-10 11:50:25 +0100

    first one

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM debian:jessie
+
+WORKDIR /app
+
+RUN apt-get update \
+&& apt-get install -y curl \
+&& rm -rf /var/lib/apt/lists/*
+
+RUN curl -LO "https://nodejs.org/dist/latest/node-v11.9.0-linux-x64.tar.gz" \
+&& tar -xzf node-v11.9.0-linux-x64.tar.gz -C /usr/local --strip-components=1 \
+&& rm node-v11.9.0-linux-x64.tar.gz
+
+ENV MYSQL_PWD azerty123
+RUN echo "mysql-server mysql-server/root_password password $MYSQL_PWD" | debconf-set-selections
+RUN echo "mysql-server mysql-server/root_password_again password $MYSQL_PWD" | debconf-set-selections
+
+
+RUN apt-get update && apt-get install -y mysql-server && apt-get install mysql-client
+
+ADD package.json ./
+
+RUN npm install 
+
+ADD . /app/
+
+CMD find /var/lib/mysql -type f -exec touch {} \;&& /etc/init.d/mysql start / && mysql -u root -e "CREATE DATABASE tp_db;USE tp_db;CREATE TABLE indexCpt (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,cpt INT )" && node index.js
---
+++ index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var obj = {
+};
+
+var con = mysql.createConnection({
+   host: "localhost",
+   user: "root",
+   password: "azerty123",
+   database :"tp_db"
+ });
+ 
+ con.connect(function(err) {
+   if (err) throw err;
+   console.log("Connected!");
+ });
+ app.use(function (req, res, next) {
+   res.header('Access-Control-Allow-Credentials', true);
+   res.header('Access-Control-Allow-Origin', req.headers.origin);
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+   res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+   next();
+});
+ app.get('/', function (req, res) {
+   console.log("DEBUT get Index.....");
+   con.query('SELECT * FROM indexCpt', (err,rows) => {
+      if(err) throw err;
+      console.log('Get Index done!\n');
+      if(rows[0] == null){
+         var newCpt = 0;
+         var sql = "INSERT INTO indexCpt (cpt) values ("+newCpt+")";
+         console.log('Debut Insert Index!\n');
+         con.query(sql, function (err, result) {
+            if (err) throw err;
+            res.send("<h1> Le compteur est null </h1>")
+            console.log('Update Index Done');
+         });
+      } else{
+         var newCpt = parseInt(rows[0]['cpt']) + 1;
+         var sql = "UPDATE indexCpt SET cpt='"+newCpt+"'";
+         console.log('Debut Update Index!\n');
+         con.query(sql, function (err, result) {
+            if (err) throw err;
+            res.send("<h1>Nouvelle valeur : " + newCpt + '</h1>')
+            console.log('Update Index Done');
+         });
+      }
+      
+   });
+   console.log("FIN get Index.....");
+
+});
+
+var server = app.listen(80, function () {
+   var port = server.address().port
+   console.log("le serveur tourne sur  %s", port)
+})
